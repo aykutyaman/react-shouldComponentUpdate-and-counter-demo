81cbe490b914da027676eadeef754759f5fba702
Aykut Yaman <yaman.ayk@gmail.com>
2016-08-22 16:58:39 +0300

    perf added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Perf from 'react-addons-perf';
+
+window.Perf = Perf;
 
 class App extends Component {
   /**
